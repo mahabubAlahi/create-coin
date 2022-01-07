4e37348255d0f8fd0c03f9b996ef409a78d299bc
mahabubAlahi <mahabubalahi9531@gmail.com>
2022-01-07 14:49:09 +0600

    passing two element sender_mnemonic and receiver_mnemonic and logic change

--- create_add.py
+++ create_add.py
@@ -18,36 +18,45 @@ app.add_middleware(
 )
 
 class Item(BaseModel):
-    mnemonic: str
+    sender_mnemonic: str
+    receiver_mnemonic: str
 
 @app.post('/send-coin')
 def send_coin(item: Item):
     #Mnemonic to Root key Generate
     
     mnemon = Mnemonic('english')
-    if not mnemon.check(item.mnemonic):
-        raise HTTPException(status_code=400, detail='Invaild Mnemonic')
-    seed = mnemon.to_seed(item.mnemonic)
-    root_key = bip32utils.BIP32Key.fromEntropy(seed)
-    root_private_wif = root_key.WalletImportFormat()
+    if not mnemon.check(item.sender_mnemonic):
+        raise HTTPException(status_code=400, detail='Invaild Sender Mnemonic')
+    if not mnemon.check(item.receiver_mnemonic):
+        raise HTTPException(status_code=400, detail='Invaild Receiver Mnemonic')
+    #Sender Seed
+    sender_seed = mnemon.to_seed(item.sender_mnemonic)
+    sender_root_key = bip32utils.BIP32Key.fromEntropy(sender_seed)
+    sender_root_private_wif = sender_root_key.WalletImportFormat()
 
-    value = random.randint(0,10000)
+    #Receiver Seed
+    receiver_seed = mnemon.to_seed(item.receiver_mnemonic)
+    receiver_root_key = bip32utils.BIP32Key.fromEntropy(receiver_seed)
+    receiver_root_private_wif = receiver_root_key.WalletImportFormat()
+
+    #value = random.randint(0,10000)
 
     #Child Key Generate
-    child_key = root_key.ChildKey(0).ChildKey(0)
-    child_private_wif = child_key.WalletImportFormat()
+    #child_key = root_key.ChildKey(0).ChildKey(0)
+    #child_private_wif = child_key.WalletImportFormat()
 
     #Generate Wallet
-    my_key = PrivateKeyTestnet(root_private_wif)
-    my_key2 = PrivateKeyTestnet(child_private_wif)
+    sender_my_key = PrivateKeyTestnet(sender_root_private_wif)
+    receiver_my_key = PrivateKeyTestnet(receiver_root_private_wif)
 
     #Sender Address
-    to_address = my_key2.address
-    transfer_balance = my_key.get_balance('btc')
+    sender_address = sender_my_key.address
+    #Receiver Address
+    receiver_address = receiver_my_key.address
     try:
-        tx_hash = my_key.send([(to_address, 0.00001, 'btc')])
+        tx_hash = sender_my_key.send([(receiver_address, 0.00001, 'btc')])
     except Exception as e:
-        raise HTTPException(status_code=400, detail='Insufficient Fund - '+ my_key.address + ' (Please store enough fund to the address)')
-    my_key_final = PrivateKeyTestnet(root_private_wif)
-    my_key2_final = PrivateKeyTestnet(child_private_wif)
-    return {'detail': 'Successfully sent 0.00001 btc from '+ my_key.address + ' to ' + to_address,'sender_address': my_key.address ,'sender_balance': my_key_final.get_balance('btc'), 'receiver_address': to_address}
+        raise HTTPException(status_code=400, detail='Insufficient Fund - '+ sender_address + ' (Please store enough fund to the sender address)')
+    
+    return {'detail': 'Successfully sent 0.00001 btc from '+ sender_address + ' to ' + receiver_address,'sender_address': sender_address ,'sender_balance': sender_my_key.get_balance('btc'), 'receiver_address': receiver_address, 'receiver_balance': receiver_my_key.get_balance('btc')}
